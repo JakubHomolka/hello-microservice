3400e92d05fc51ff153a993757b94c52d1e248a2
HomolkaJakub <homolka.jakubcz@gmail.com>
2024-11-22 11:27:48 +0100

    Added hostname

--- hello-service.py
+++ hello-service.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 @app.route('/hello/<name>')
 def hello(name):
-    message = f'Hello {name}! How are you?'
-    response = f'{{message: "{message}"}}\r\n'
+    hostname = socket.gethostbyname()
+    message = f'Hello {name}! My name is {hostname}.'
+    response = f'{{ message: "{message}"}}\r\n'
     code = 200
     return response, code, {'Content-Type': 'application/json'}
 
